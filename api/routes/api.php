10db1f42d9e99f5e35ff67adc39f4d5993fdc58e
Aleksey Belchenkov <u608110@gmail.com>
2021-06-26 17:39:59 +0400

    Prepare env

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
-Route::group(['middleware' => ['auth:api']], function () {
-
-});
-
-Route::group(['middleware' => ['guest:api']], function () {
-    Route::post('register', 'Auth\RegisterController@register');
+Route::get('/', function() {
+    return response()->json([
+        'message' => 'DesignHouse API...'
+    ], 200);
 });
