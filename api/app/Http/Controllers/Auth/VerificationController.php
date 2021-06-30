03c25491ce09bd96e698451a124b104cc5a5fd5f
Aleksey Belchenkov <u608110@gmail.com>
2021-06-30 14:24:37 +0400

    Email Verification: Activating the user's account

--- api/app/Http/Controllers/Auth/VerificationController.php
+++ api/app/Http/Controllers/Auth/VerificationController.php
@@ -19,7 +19,7 @@ class VerificationController extends Controller
     public function __construct()
     {
         $this->middleware('signed')->only('verify');
-        $this->middleware('throttle:6,1')->only('verify', 'resend');
+        $this->middleware('throttle:16,1')->only('verify', 'resend');
     }
 
     public function verify(Request $request, User $user)
--- api/routes/api.php
+++ api/routes/api.php
@@ -17,6 +17,6 @@ Route::group(['middleware' => ['auth:api']], function () {
 // Routes for guest users
 Route::group(['middleware' => ['guest:api']], function () {
     Route::post('register', 'Auth\RegisterController@register');
-    Route::post('verification/verify', 'Auth\VerificationController@verify')->name('verification.verify');
+    Route::post('verification/verify/{user}', 'Auth\VerificationController@verify')->name('verification.verify');
     Route::post('verification/resend', 'Auth\VerificationController@resend');
 });
