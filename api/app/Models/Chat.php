9398622a60f80e2ecd4833ba4e16ce585169c415
Aleksey Belchenkov <u608110@gmail.com>
2021-08-02 10:55:20 +0400

    Setup chat & message model

---
+++ api/app/Models/Chat.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Chat extends Model
+{
+    public function participants(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'participants');
+    }
+
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function getLatestMessageAttribute(): HasMany
+    {
+        return $this->messages()->latest()->first();
+    }
+
+    public function isUnreadForUser(int $user_id): bool
+    {
+        return (bool)$this->messages()
+            ->whereNull('last_read')
+            ->where('user_id', '<>', $user_id)
+            ->count();
+    }
+
+    public function markAsReadForUser(int $user_id): void
+    {
+        $this->messages()
+            ->whereNull('last_read')
+            ->where('user_id', '<>', $user_id)
+            ->update([
+                'last_read' => Carbon::now()
+            ]);
+    }
+}
+
---
+++ api/app/Models/Message.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Message extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'chat_id',
+        'body',
+        'last_read'
+    ];
+
+    protected $touches = ['chat'];
+
+    public function chat(): BelongsTo
+    {
+        return $this->belongsTo(Chat::class);
+    }
+
+    public function sender(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
